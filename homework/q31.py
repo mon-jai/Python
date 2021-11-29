e05c7acabddc4f6cd967b5262b852093dba309db
mon-jai <91261297+mon-jai@users.noreply.github.com>
2021-11-30 01:24:57 +0800

    .

--- homework/q31.py
+++ homework/q31.py
@@ -4,8 +4,8 @@ from typing import List, Tuple, Union
 def C(m: int, count: int = 0) -> Tuple[Union[int, None], int]:
     return (
         (None, count) if m == 1 or m == 0
-        else (C(m // 2, count + 1)) if m % 2 == 0
-        else (C((m + 1) // 2, count + 1))
+        else C(m // 2, count + 1) if m % 2 == 0
+        else C((m + 1) // 2, count + 1)
     )
 
 
