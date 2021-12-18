5b441e9df5930d037a22fe1566f573e639f806b8
mon-jai <91261297+mon-jai@users.noreply.github.com>
2021-12-18 15:14:54 +0800

    Update q37.py

--- homework/q37.py
+++ homework/q37.py
@@ -1,34 +1,29 @@
 from typing import List, Tuple
 
-Data = List[Tuple[int, int]]
-
-
-def merge(data: Data) -> Data:
-    result = data.copy()
-    merged = False
-
-    for index1, tuple1 in enumerate(result):
-        for tuple2 in result:
-            if (
-                id(tuple2) != id(tuple1) and
-                tuple1[0] <= tuple2[0] <= tuple1[1]
-            ):
-                result.remove(tuple2)
-                result[index1] = (min(tuple1[0], tuple2[0]),
-                                  max(tuple1[1], tuple2[1]))
-                merged = True
-
-    return result if merged == False else merge(result)
-
 
 def main():
-    data: Data = []
+    data: List[Tuple[int, int]] = []
 
     for _ in range(int(input())):
         numbers = [int(s) for s in input().split(',')]
         data.append((numbers[0], numbers[1]))
 
-    print('\n'.join([f'{tuple[0]},{tuple[1]}' for tuple in merge(data)]))
+    while True:
+        merged = False
+        for index1, tuple1 in enumerate(data):
+            for tuple2 in data:
+                if (
+                    id(tuple2) != id(tuple1) and
+                    tuple1[0] <= tuple2[0] <= tuple1[1]
+                ):
+                    data.remove(tuple2)
+                    data[index1] = (min(tuple1[0], tuple2[0]),
+                                    max(tuple1[1], tuple2[1]))
+                    merged = True
+        if not merged:
+            break
+
+    print('\n'.join([f'{tuple[0]},{tuple[1]}' for tuple in data]))
 
 
 main()
