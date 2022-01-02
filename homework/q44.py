71f1112b3a9112f22719470f2f719f70ddfa8a43
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-01-03 00:00:16 +0800

    Update q44.py

--- homework/q44.py
+++ homework/q44.py
@@ -21,39 +21,30 @@ def main():
         # 特別獎
         if invoice_id == special_prize_winning_number:
             prize_moneys.append(10000000)
-            continue
         # 特獎
         elif invoice_id == grand_prize_winning_number:
             prize_moneys.append(2000000)
-            continue
         # 頭獎
         elif invoice_id in first_prize_winning_numbers:
             prize_moneys.append(200000)
-            continue
         # 二獎
         elif matches_last_nth_digits_with_first_prize_winning_numbers(invoice_id, 7):
             prize_moneys.append(40000)
-            continue
         # 三獎
         elif matches_last_nth_digits_with_first_prize_winning_numbers(invoice_id, 6):
             prize_moneys.append(10000)
-            continue
         # 四獎
         elif matches_last_nth_digits_with_first_prize_winning_numbers(invoice_id, 5):
             prize_moneys.append(4000)
-            continue
         # 五獎
         elif matches_last_nth_digits_with_first_prize_winning_numbers(invoice_id, 4):
             prize_moneys.append(1000)
-            continue
         # 六獎
         elif matches_last_nth_digits_with_first_prize_winning_numbers(invoice_id, 3):
             prize_moneys.append(200)
-            continue
         # 增開六獎
         elif any(invoice_id[-3:] == suffix for suffix in sixth_prize_winning_numbers):
             prize_moneys.append(200)
-            continue
 
     print(sum(prize_moneys))
 
